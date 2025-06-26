f80cef153de202b07854a92261ddbfe66e45fd31
MayAshke <118209149+MayAshke@users.noreply.github.com>
2025-06-26 16:10:04 +0300

    edit server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import app from './app';
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
